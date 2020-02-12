9128aa4ebfa58031fa0a5201e4881a150ad74a18
joon <jtj308@naver.com>
2020-02-13 01:45:05 +0900

    Using API to view and delete the data

--- BookListApp/Startup.cs
+++ BookListApp/Startup.cs
@@ -26,6 +26,7 @@ namespace BookListApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllersWithViews();
             services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
@@ -52,6 +53,7 @@ namespace BookListApp
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
